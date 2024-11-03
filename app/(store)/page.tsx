802cd7741ef0d6b7efdc4c6a29e6e8196f6e77b4
Edrisa Leigh <edrisaleigh@Edrisas-MacBook-Pro.local>
2024-11-03 00:45:39 -0700

    Implement the header section and all its addons, clerk authentication

---
+++ app/(store)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h3>Sanity e-commerce build 123 this is just amazing</h3>
+      <Button>Click me</Button>
+    </div>
+  );
+}
